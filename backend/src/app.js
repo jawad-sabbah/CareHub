857185a37106074a9ae33ad1004a0a19b7259f88
Jawad sabbah <sabbah.m.jawad@gmail.com>
2026-05-26 18:42:59 +0300

    connect the server to database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import pool from './config/db.js';
 
 dotenv.config();
 
@@ -9,6 +9,17 @@ const PORT = process.env.PORT;
 
 
 
+async function start() {
+    try {
+        await pool.connect();
+        console.log('Connected to the database');
+    }
+    catch (error) {
+        console.error('Error connecting to the database', error);
+    } 
+}
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
+    start();
 });
