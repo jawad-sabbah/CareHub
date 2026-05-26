44a10cbc209d48fae225a620e804a7dd082fba5b
Jawad sabbah <sabbah.m.jawad@gmail.com>
2026-05-26 18:29:53 +0300

    intialize the express server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,14 @@
-a
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
