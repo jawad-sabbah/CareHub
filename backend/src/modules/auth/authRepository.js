546a7681a27852c4f6734207f547f41c6f1cda2a
Jawad sabbah <sabbah.m.jawad@gmail.com>
2026-06-25 22:09:29 +0300

    fix bugs

--- backend/src/modules/auth/authRepository.js
+++ backend/src/modules/auth/authRepository.js
@@ -61,6 +61,12 @@ class AuthRepository {
     return result.rows[0];
   }
 
+
+  async getUserById(id) {
+  const query = `SELECT * FROM users WHERE id = $1`;
+  const result = await pool.query(query, [id]);
+  return result.rows[0];
+}
   
 
 }
